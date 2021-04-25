af5cedb2010459f0862aaca336d25df715ba4be7
yogesh147 <yogeshch147@gmail.com>
2021-04-25 12:42:16 +0530

    life 1

--- smartGit/src/test/java/Test1.java
+++ smartGit/src/test/java/Test1.java
@@ -1,8 +1,8 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Tes2");
-		System.out.println("Hello Tes2 "  + 1);
+	
+		System.out.println(" Master ");
 
 	}
 
--- smartGit/src/test/java/Test2.java
+++ smartGit/src/test/java/Test2.java
@@ -1,9 +1,8 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Tes1");
-
-		System.out.println("Hello Tes1" + 21);
+		
+		System.out.println(" Master 2 ");
 
 	}
 
