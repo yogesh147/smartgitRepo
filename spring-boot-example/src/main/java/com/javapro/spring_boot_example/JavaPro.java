ff298df77faed4812bc46452201c1817e5061a84
yogesh147 <yogeshch147@gmail.com>
2021-07-18 14:17:15 +0530

    Create spring boot application

---
+++ spring-boot-example/src/main/java/com/javapro/spring_boot_example/JavaPro.java
@@ -0,0 +1,12 @@
+package com.javapro.spring_boot_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPro {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPro.class, args);
+	}
+}
